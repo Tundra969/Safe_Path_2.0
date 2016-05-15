6080950f996534768ef0c0d30524e8de54a809e9
Rachel Mahoney <rachel.mahoney@target.com>
2016-05-15 11:41:40 -0500

    minneapolis doctor results

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -11,9 +11,9 @@ angular.module('testProjectApp')
     var self = this;
 
     this.testFunc = function() {
-       $http.get('https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=psychiatrist&location=37.4419%2C-122.1430%2C100&user_location=37.4419%2C-122.1430&skip=0&limit=10&user_key=41718e01b8fe03f28c973af6d766278a').then(function(response){
-        console.log(response.data.data[0].practices);
-        self.results = response.data.data[0].practices;
+       $http.get('https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=mental-health-counselor&location=44.9778%2C-93.2650%2C100&user_location=44.9778%2C-93.2650&skip=0&limit=10&user_key=41718e01b8fe03f28c973af6d766278a').then(function(response){
+        console.log(response.data.data[1].practices);
+        self.results = response.data.data[1].practices;
         self.isHidden = false;
       });
     };
